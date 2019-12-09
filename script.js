08ea76e128fe90a309d0903ce96bdc93135b15d2
Nicolas Guerrero <saynaysurf@gmail.com>
2019-12-08 16:44:24 -0800

    second commit

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ document.addEventListener("DOMContentLoaded",function() {
     var button = document.querySelector("#submit");
     var memes = document.querySelector(".memes");
     var form = document.querySelector("form");
-    
+    var inputImage = document.querySelectorAll("input")[0];
+    var inputTop = document.querySelectorAll("input")[1];
+    var inputBottom = document.querySelectorAll("input")[2];
+
     form.addEventListener("submit", function(event) {
-        var inputImage = document.querySelectorAll("input")[0];
-        var inputTop = document.querySelectorAll("input")[1];
-        var inputBottom = document.querySelectorAll("input")[2];
-        event.preventDefault();
         
-    if (inputImage.value === "" )   {
+      event.preventDefault();
+        
+      if (inputImage.value === "" )   {
         alert("Please enter an image url!");
-    } else {
+      } else {
         var meme = document.createElement("div");
         meme.className = "meme";
 
@@ -22,52 +23,29 @@ document.addEventListener("DOMContentLoaded",function() {
 
         var topText = document.createElement("div");
         topText["innerText"] = inputTop["value"];
-        topText.style.position = "absolute";
-        topText.style.top = 0;
-        topText.className = "textmeme";
+        topText.className = "textTop";
 
         var bottomText = document.createElement("div");
         bottomText["innerText"] = inputBottom["value"];
-        bottomText.style.position = "absolute";
-        bottomText.style.bottom = 0;
-        bottomText.className = "textmeme";
-
+        bottomText.className = "textBottom";
+        
         imgDiv.appendChild(image);
-
         meme.appendChild(imgDiv);
         meme.appendChild(topText);
         meme.appendChild(bottomText);
 
-        
+        reset();
 
         memes.appendChild(meme);
 
-        reset();
+        meme.addEventListener("click", function(e) {
+            this.parentNode.removeChild(this);
+         });
         
     }
         
-    }); 
-    memes.addEventListener("mouseover", function() {
- 
-         if (event.target.className === "meme") {
-             var memeTarget = event.target;
-             var cancelButton = document.createElement("button");
-             alert("hello");
-            // memeTarget.remove();
-             cancelButton.style.position = "absolute";
-             cancelButton.right = 0;
-             cancelButton.innerText = "Remove";
-             event.target.appendChild(cancelButton);
-             
-             cancelButton.addEventListener("click", function() {
-                 memeTarget.remove();
-             });
- 
-         }
-     });
-
-
-    
+    });
+   
     function reset() {
         var inputs = document.querySelectorAll("input");
         for (var i = 0; i < inputs.length; i++) {
